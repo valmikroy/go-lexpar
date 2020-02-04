48f511bc41adab5538af1a536758ab45690d5bf2
abhsawan <abhsawan@amazon.com>
2020-02-04 15:40:23 -0800

    more test cases

--- pkg/parser_test.go
+++ pkg/parser_test.go
@@ -7,9 +7,27 @@ import (
 	"testing"
 )
 
-func TestParsing(t *testing.T) {
+func TestFruParsing(t *testing.T) {
 
-	content, _ := ioutil.ReadFile("text.txt")
+	content, _ := ioutil.ReadFile("resource/fru.out")
+	input := string(content)
+	var r io.Reader = strings.NewReader(input)
+	var p *Parser = NewParser(r)
+	p.Parse()
+}
+
+func TestLanParsing(t *testing.T) {
+
+	content, _ := ioutil.ReadFile("resource/lan_print.out")
+	input := string(content)
+	var r io.Reader = strings.NewReader(input)
+	var p *Parser = NewParser(r)
+	p.Parse()
+}
+
+func TestChassisStatusParsing(t *testing.T) {
+
+	content, _ := ioutil.ReadFile("resource/chassis_status.out")
 	input := string(content)
 	var r io.Reader = strings.NewReader(input)
 	var p *Parser = NewParser(r)
