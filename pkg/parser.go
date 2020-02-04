dc9138e18fab7b891c26574fa82b89b433667732
Valmik Roy <abhsawan@ip-10-200-212-119.us-west-2.compute.internal>
2020-02-04 21:48:00 +0000

    Adding parser update

--- pkg/parser.go
+++ pkg/parser.go
@@ -38,30 +38,16 @@ func (p *Parser) scan() (tok Token, lit string) {
 }
 
 func (p *Parser) unscan() { p.buf.isUnscanned = true }
+func (p *Parser) carriageReturn() int {
+	cnt := p.s.pos
+	p.s.pos = 0
+	return cnt
+}
 
 func (p *Parser) parseNewline() bool {
 
 	tok, _ := p.scan()
 	if tok == NEWLINE {
-		return true
-	}
-	p.unscan()
-	return false
-}
-
-func (p *Parser) parseILLEGAL() error {
-
-	tok, l := p.scan()
-	if tok == ILLEGAL {
-		return fmt.Errorf("Illegal token '%s'\n", l)
-	}
-	return nil
-}
-
-func (p *Parser) parseEOF() bool {
-
-	tok, _ := p.scan()
-	if tok == EOF {
 		return true
 	}
 	p.unscan()
@@ -84,7 +70,6 @@ func (p *Parser) parseVal() *string {
 func (p *Parser) parseKey() *string {
 	var key *string
 	tok, lit := p.scan()
-	fmt.Println(lit)
 	if tok == KEY {
 		k := lit
 		key = &k
@@ -96,42 +81,57 @@ func (p *Parser) parseKey() *string {
 }
 
 func (p *Parser) Parse() {
-	//	var r R = make(R)
-
-	var fru Fru = make(Fru, 0)
-	unit := make(map[string]string)
+	//var fru Fru = make(Fru, 0)
+	//unit := make(map[string]string)
 	for {
 
-		err := p.parseILLEGAL()
-		if err != nil {
-			fmt.Println(err)
-			break
-		}
-
 		k := p.parseKey()
+		//fmt.Println(k)
 
 		v := p.parseVal()
+		//fmt.Println(*v)
 
-		if k != nil && p.parseNewline() {
-			//fmt.Printf("%s : %s\n", *k, *v)
-			unit[*k] = *v
-			continue
-		}
+		p.parseNewline()
 
-		if k == nil && v == nil {
-			if p.parseNewline() {
-				fmt.Println("Starting new section")
-				fru = append(fru, unit)
-				//j, _ := json.Marshal(unit)
-				//fmt.Println(string(j))
-				unit = make(map[string]string)
-				continue
-			} else {
+		/*
+			if p.parseEOF() {
 				break
 			}
-		}
-	}
+		*/
 
+		pos := p.carriageReturn()
+
+		if pos == 0 {
+			break
+		} else if pos == 3 {
+			fmt.Println("XXXXXXXXXXXXXXXXXXXXXX")
+		} else {
+			fmt.Printf("%s : %s\n", *k, *v)
+		}
+
+		/*
+			if k != nil && p.parseNewline() {
+				fmt.Printf("%s : %s\n", *k, *v)
+				unit[*k] = *v
+				p.carriageReturn()
+				continue
+			}
+
+			if k == nil && v == nil {
+				if p.parseNewline() {
+
+					fmt.Println("XXXXXXXXXXXXXXXXXXXXXX")
+					fru = append(fru, unit)
+					//j, _ := json.Marshal(unit)
+					//fmt.Println(string(j))
+					unit = make(map[string]string)
+					continue
+				} else {
+					break
+				}
+			}
+		*/
+	}
 	//fmt.Printf("%v", fru)
 	//j, _ := json.Marshal(fru)
 	//fmt.Println(string(j))
--- pkg/tokens.go
+++ pkg/tokens.go
@@ -4,33 +4,32 @@ package lexpar
 type Token int
 
 const DELIMITER rune = ':'
-const KEY_LENGTH int = 23
 
 // kinds of token
 const (
-        ILLEGAL Token = iota
-        EOF
-        WS
-        NEWLINE
+	ILLEGAL Token = iota
+	EOF
+	WS
+	NEWLINE
 
-        KEY
-        VAL
+	KEY
+	VAL
 )
 
 type TokenInstance struct {
-        Type    Token
-        Literal string
+	Type    Token
+	Literal string
 }
 
 func isWhitespace(ch rune) bool {
-        return ch == ' ' || ch == '\t'
+	return ch == ' ' || ch == '\t'
 }
 func isNewLine(ch rune) bool {
-        return ch == '\n'
+	return ch == '\n'
 }
 
 func isDelimiter(ch rune) bool {
-        return ch == DELIMITER
+	return ch == DELIMITER
 }
 
 var eof = rune(0)
